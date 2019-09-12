109bcd6da80e2d7cd465150e431c487073849406
藤原未来 <www.motimoti12@gmail.com>
2019-09-12 19:36:06 +0900

    users コントローラ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :cofigure_permitted_parameters, if: :devise_controller?
+  
+  protected
+
+  def cofigure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
