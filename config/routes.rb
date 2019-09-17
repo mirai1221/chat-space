a429b02445fb645a759a51e1abe5baad8c7469d1
藤原未来 <www.motimoti12@gmail.com>
2019-09-17 19:34:57 +0900

    グループ機能のルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root  'messages#index'
-   resource :users, only: [:edit, :update]
+   resource :users, only: [:index,:edit, :update]
+   resource :groups, only: [:new, :create,:edit,:update] do
+    resource :messages, only: [:index,:create]
 
 
 end
