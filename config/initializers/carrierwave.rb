33925617b6c403a860b38d6650292f7d86ee5d56
藤原未来 <www.motimoti12@gmail.com>
2019-10-09 20:27:13 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'iamgechat-space'
+  config.fog_directory  = 'imagechat-space'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/imagechat-space'
 end
